bbd8a42a69e8ebc2bd56aed107efbbaff78a9d95
Grace-254 <gracekavutu6@gmail.com>
2025-09-04 12:59:23 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -56,4 +56,5 @@ JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
 LEFT JOIN payments pay ON b.id = pay.booking_id
 WHERE b.start_date >= '2025-01-01'
+  AND pay.status = 'confirmed'
 ORDER BY b.start_date DESC;
